8afab68e4c66b6a2437bb74c94d213ec697fc7e5
Karenandrea86 <andreahueso86@gmail.com>
2023-12-22 16:54:52 -0500

    commit

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,10 +1,9 @@
-from app.auth import auth
-import app
+from flask_login import login_user, current_user, logout_user
 from flask import render_template, redirect, flash
-from flask_login import login_user, logout_user
-import wtforms
-from .forms import *
-
+from flask_login import login_required
+from app.auth import auth
+from .forms import LoginForm
+import app
 
 @auth.route('/login',
             methods=["GET",'POST'])
@@ -14,28 +13,18 @@ def login():
         #selecciona al Usuarios por username
         c =app.models.Usuarios.query.filter_by(username=form.username.data).first()
         if c is None or not c.check_password(form.password.data):
-            print('usuario no existe')
-            redirect('/auth/login')
+            flash('Usuario o contraseña incorrectos')
+            return redirect('/auth/login')
             #mensaje flask de usuario no existente
         else:
-            flash('Bienvenido al sistema')
             login_user(c, remember=True)
-            return redirect('/usuarios/listar')
+            flash('Bienvenido al sistema')
+            return redirect('/mangas/listar')
     return render_template('login.html', form = form)
 
 @auth.route('/logout')
+@login_required
 def logout():
     logout_user()
+    flash('Sesión cerrada con exito')
     return redirect('/auth/login')
-
-@auth.route('/register', methods=['GET', 'POST'])
-def register():
-    p = app.models.Usuarios()
-    form = NewUserForm()
-    if form.validate_on_submit():
-        form.populate_obj(p)
-        app.db.session.add(p)
-        app.db.session.commit()
-        flash("Usuario registrado correctamente")
-        return redirect('/')
-    return render_template('register.html', form=form, wtf=wtforms)
