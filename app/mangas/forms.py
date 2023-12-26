3b4fbba74fb6aabad3bf55a339b660006c53b306
ForestParadiseOrigen <andres.jcv.678@gmail.com>
2023-12-26 14:29:35 -0500

    Redirecciones condicionadas

--- app/mangas/forms.py
+++ app/mangas/forms.py
@@ -5,52 +5,48 @@ from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 class MangaForm():
     title = StringField(
-                        "Ingrese título del manga" ,
-                        validators=[
-                            InputRequired(message='Título requerido')
-                                    ]
-                        
-                        
-                        )
-    description = StringField("Ingrese la descripción del manga",
-                         validators=[
-                             InputRequired(
-                                 message="Descripción requerido"
-                                 )
-                                     ]
-                        )
+        "Ingrese título del manga",
+        validators=[
+            InputRequired(message='Título requerido')
+        ]
+    )
+    description = StringField(
+        "Ingrese la descripción del manga",
+        validators=[
+            InputRequired(message="Descripción requerido")
+        ]
+    )
     return_date = DateField(
-                        "Ingrese la fecha de devolución" ,
-                        validators=[
-                            DataRequired(message='Fecha requerida')
-                                    ]
-                        
-                        
-                        )
+        "Ingrese la fecha de devolución" ,
+        validators=[
+            DataRequired(message='Fecha requerida')
+        ]
+    )
     status = SelectField(
-                        "Ingrese el estado del manga" ,
-                        choices = [
-                            "Alquilado",
-                            "Disponible"
-                        ],
-                        validators=[
-                            InputRequired(message='Estado requerido')
-                                    ]                        
-                        )
+        "Ingrese el estado del manga",
+        choices = [
+            "Alquilado",
+            "Disponible"
+        ],
+        validators=[
+            InputRequired(message='Estado requerido')
+        ]                        
+    )
     price = IntegerField(
-                        "Ingrese el precio del manga" ,
-                        validators=[
-                            InputRequired(message='Precio requerido')
-                                    ]
-                        )
+        "Ingrese el precio del manga" ,
+        validators=[
+            InputRequired(message='Precio requerido')
+        ]
+    )
     image_path = FileField (label="Ingrese la imagen promocional",
-                        validators=[FileRequired(message="Se requiere la imagen promocional"),
-                                    FileAllowed(
-                                        ["jpg","png"],
-                                        message="Solo se aceptan imagenes"
-                                    )
-                                    ]
-                       )
+        validators=[
+            FileRequired(message="Se requiere la imagen promocional"),
+            FileAllowed(
+                ["jpg","png"],
+                message="Solo se aceptan imagenes"
+            )
+        ]
+    )
 class NewMangaForm(FlaskForm, MangaForm):
     submit = SubmitField("Guardar")
     
